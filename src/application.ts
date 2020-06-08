52312afeb93ee4599147dd58f6ad264aac0cc13f
WillRy <wiribasczky@gmail.com>
2020-06-08 18:12:39 -0300

    Configuração de server do RabbitMQ
    - Mudado estrutura padrão de server, para suportar multiplos servers.
    - Migrado de RestApplication para Application
    - Criado componente customizado do RestExplorer, devido mudanças na estrutura
      do servidor, o que causou imcompatibilidades de tipos.
    - Iniciado servidor para comunicaçao do RabbitMQ

--- src/application.ts
+++ src/application.ts
@@ -1,26 +1,26 @@
 import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
-import {
-  RestExplorerBindings,
-  RestExplorerComponent,
-} from '@loopback/rest-explorer';
+import {Application, ApplicationConfig} from '@loopback/core';
 import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
+import {RestComponent, RestServer} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
+import {RabbitmqServer} from './servers';
+import {RestExplorerComponent} from './components';
+import {RestExplorerBindings} from '@loopback/rest-explorer';
 
 export class MicroCatalogApplication extends BootMixin(
-  ServiceMixin(RepositoryMixin(RestApplication)),
+  ServiceMixin(RepositoryMixin(Application)),
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
 
     // Set up the custom sequence
-    this.sequence(MySequence);
-
+    options.rest.sequence = MySequence;
+    this.component(RestComponent);
+    const restServer = this.getSync<RestServer>('servers.RestServer');
     // Set up default home page
-    this.static('/', path.join(__dirname, '../public'));
+    restServer.static('/', path.join(__dirname, '../public'));
 
     // Customize @loopback/rest-explorer configuration here
     this.bind(RestExplorerBindings.CONFIG).to({
@@ -38,5 +38,7 @@ export class MicroCatalogApplication extends BootMixin(
         nested: true,
       },
     };
+
+    this.servers([RabbitmqServer]);
   }
 }
