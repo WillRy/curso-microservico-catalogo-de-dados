f4e57c8085a4ed8c1bf5ab289e0a73b2c2890b7e
William R <wiribasczky@gmail.com>
2020-09-26 06:33:25 -0300

    Atualizado services de sincronia para fazerem uso da validação
    Problema/Proposito
    
    - Atualizado classes de services de sincronização com o ElasticSearch para
    fazerem uso das validações de JSON Schema

--- src/services/base-model-sync.service.ts
+++ src/services/base-model-sync.service.ts
@@ -2,6 +2,7 @@ import {bind, /* inject, */ BindingScope} from '@loopback/core';
 import {DefaultCrudRepository} from '@loopback/repository';
 import {Message} from 'amqplib';
 import {pick} from 'lodash';
+import {ValidatorService} from './validator.service';
 
 export interface SyncOptions {
   repo: DefaultCrudRepository<any, any>;
@@ -11,11 +12,17 @@ export interface SyncOptions {
 
 @bind({scope: BindingScope.SINGLETON})
 export abstract class BaseModelSyncService {
+  constructor(private validateService: ValidatorService) {}
+
   protected async sync({repo, data, message}: SyncOptions) {
     const action = this.getAction(message);
     const entity = this.createEntity(data, repo);
     switch (action) {
       case 'created':
+        await this.validateService.validate({
+          data: entity,
+          entityClass: repo.entityClass,
+        });
         await repo.create(entity);
         break;
       case 'updated':
@@ -44,6 +51,11 @@ export abstract class BaseModelSyncService {
     entity: any,
   ) {
     const exists = await repo.exists(id);
+    await this.validateService.validate({
+      data: entity,
+      entityClass: repo.entityClass,
+      ...(exists && {options: {partial: true}}),
+    });
     return exists ? repo.updateById(id, entity) : repo.create(entity);
   }
 }
--- src/services/cast-member-sync.service.ts
+++ src/services/cast-member-sync.service.ts
@@ -1,17 +1,24 @@
-import {bind, /* inject, */ BindingScope} from '@loopback/core';
+import {
+  bind,
+  /* inject, */ BindingScope,
+  inject,
+  service,
+} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {Message} from 'amqplib';
 import {rabbitmqSubscribe} from '../decorators/rabbitmq-subscribe.decorator';
 import {CastMemberRepository} from '../repositories/cast-member.repository';
 import {BaseModelSyncService} from './base-model-sync.service';
+import {ValidatorService} from './validator.service';
 
 @bind({scope: BindingScope.SINGLETON})
 export class CastMemberSyncService extends BaseModelSyncService {
   constructor(
     @repository(CastMemberRepository)
     private repo: CastMemberRepository,
+    @service(ValidatorService) private validator: ValidatorService,
   ) {
-    super();
+    super(validator);
   }
 
   @rabbitmqSubscribe({
