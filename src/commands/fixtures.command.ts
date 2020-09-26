bedfd17a7cfcfb5489daab0bb2d3f38a575f40fc
William R <wiribasczky@gmail.com>
2020-09-26 06:34:22 -0300

    Atualizado comando de fixtures para fazer uso da validação
    Problema/Proposito
    
    - Atualizado comando de fixtures para fazer uso da validação de JSON Schema

--- src/commands/fixtures.command.ts
+++ src/commands/fixtures.command.ts
@@ -5,6 +5,7 @@ import {Esv7DataSource} from '../datasources';
 import fixtures from '../fixtures/index';
 import {DefaultCrudRepository} from '@loopback/repository';
 import {Client} from 'es7';
+import {ValidatorService} from '../services/validator.service';
 
 export class FixturesCommand {
   static command = 'fixtures';
@@ -19,10 +20,18 @@ export class FixturesCommand {
     console.log(chalk.green('Delete all documents'));
     await this.deleteAllDocuments();
 
+    const validator = this.app.getSync<ValidatorService>(
+      'services.ValidatorService',
+    );
+
     for (const fixture of fixtures) {
       const repository = this.getRepository<DefaultCrudRepository<any, any>>(
         fixture.model,
       );
+      await validator.validate({
+        data: fixture.fields,
+        entityClass: repository.entityClass,
+      });
       await repository.create(fixture.fields);
     }
 
